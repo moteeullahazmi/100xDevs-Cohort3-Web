a52ca1159751179e3ac37b036e9af1989ca52d8d
Moteeullah Azmi <moteeullahazmi@gmail.com>
2024-08-27 10:12:20 +0000

    azmi

--- week04/web2/chalk.js
+++ week04/web2/chalk.js
@@ -4,4 +4,4 @@ console.log(chalk.green('Success! Your operation was successful.'));
 console.log(chalk.red('Error! Something went wrong.'));
 console.log(chalk.yellow('Warning! Please check your inputs.'));
 console.log(chalk.bgCyan.black('Info: This is some informational text.'));
-console.log(chalk.bold.underline('Important Note: Please read this carefully.'));
+console.log(chalk.bold.underline('Important Note: Please read this carefully...'));
