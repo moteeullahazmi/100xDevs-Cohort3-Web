e0ce0cf6bbac330883be046fa9877097c64295b7
moteeullahazmi <61102569+moteeullahazmi@users.noreply.github.com>
2024-09-14 17:16:09 +0530

    Week 01 Complete

---
+++ Week 01 - HTML CSS JavScript/Assingment/02function.js
@@ -0,0 +1,15 @@
+// Write a function sum that finds the sum of two numbers. 
+function sum(){
+    let a=16;
+    let b=20;
+    return a+b;
+}
+console.log(sum());
+
+function addString(){
+    let st="Moteeullah"
+    let vt="Azmi"
+    return st+vt;
+}
+
+console.log(addString())
