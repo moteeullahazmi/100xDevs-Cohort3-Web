9d5a2858a72cce45ffaf99cf60e32a7d5f007053
moteeullahazmi <61102569+moteeullahazmi@users.noreply.github.com>
2024-09-14 17:20:03 +0530

    Week 05 Complete

---
+++ Week 05 - HTTP Deep Dive/5.1 - Headers, Query params and Express/middleware.js
@@ -0,0 +1,24 @@
+const express =require("express")
+
+const app = express()
+
+
+app.use(function(req, res, next) {
+    res.json({
+        message: "You are not allowed"
+    })
+})
+// azmi
+app.get("/sum", function(req, res) {
+    console.log(req.name);
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.listen(3000, azmi=>{
+    console.log("server started")
+})
---
+++ Week 05 - HTTP Deep Dive/assingments/01.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+let count = 0
+function logsMiddleware(req, res, next) {
+    const method = req.method;
+    const url = req.url;
+const currentTime = new Date();  // Try without toISOString()
+console.log(`Current time: ${currentTime}`);
+
+    count = count + 1;
+
+    console.log(`The method used is: ${method}, \nURL opened is: ${url}, \nCurrent time is: ${currentTime}, \nTotal Count is: ${count}`);
+    next();  // Move to the next middleware or route
+}
+
+
+app.use(logsMiddleware)
+
+app.get('/', (req,res)=>{
+res.json({
+    "azmi":"azmi"
+})})
+app.post('/a', (req,res)=>{
+res.json({
+    "azmi":"azmi"
+})})
+
+app.listen(3000,()=>{
+    console.log("server start")
+})
