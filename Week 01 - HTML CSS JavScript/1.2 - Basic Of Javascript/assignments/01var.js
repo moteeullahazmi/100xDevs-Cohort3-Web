695ab9c40f5025c9bab2515aafe5d8de48eca7f5
moteeullahazmi <61102569+moteeullahazmi@users.noreply.github.com>
2024-09-16 13:50:07 +0530

    updated

--- Week 01 - HTML CSS JavScript/1.2 - Basic Of Javascript/assignments/01var.js
+++
@@ -1,7 +0,0 @@
-// Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
-
-var color = "black";
-let height = "168cm";
-const likePizza = true;
-
-console.log(color, height, likePizza)
