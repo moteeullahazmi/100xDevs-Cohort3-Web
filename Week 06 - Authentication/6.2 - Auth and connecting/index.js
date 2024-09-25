4c52bae9a6d6eb2eaef586069d82cb47691cefde
moteeullahazmi <61102569+moteeullahazmi@users.noreply.github.com>
2024-09-25 18:32:23 +0530

    week 6.2 complete

--- Week 06 - Authentication/6.2 - Auth and connecting/index.js
+++ Week 06 - Authentication/6.2 - Auth and connecting/index.js
@@ -1,117 +1,93 @@
-const express = require("express")
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "azmi1234"
+const JWT_SECRET = "azmi";
 const app = express();
-app.use(express.json())
-const users = []
+app.use(express.json());
+const users = [];
 
+// Serve the homepage
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get("/",(req,res)=>{
-    res.json({
-        message:"welcome"
-    })
-})
-
-function logger (req,res,next){
-    console.log(req.method + "request came")
+// Logger middleware
+function logger(req, res, next) {
+    console.log(req.method + " request came (logger)");
     next();
 }
-app.post("/signup", logger, (req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
 
-    users.push({
-        username,password
-    })
+// Signup route
+app.post("/signup", logger, (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-    // we should check if a user with this username already exist
+    // Check if the user already exists
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.json({ message: "User already exists" });
+    }
 
-
+    users.push({ username, password });
 
     res.json({
-        message: "You are signed in"
-    })
-})
+        message: "You are signed up"
+    });
+});
 
-app.post("/signin", logger, (req,res) => {
-     const username = req.body.username
-     const password =  req.body.password
+// Signin route
+app.post("/signin", logger, (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-     let foundUser = null;
+    const foundUser = users.find(user => user.username === username && user.password === password);
 
-     for(let i =0 ; i<users.length; i++){
-        console.log(i)
-        console.log(users)
-        if(users[i].username === username && users[i].password===password){
-            console.log(i)
-            foundUser = users[i]
-        }
-     }
-
-     if(!foundUser){
-        res.json({
-            message:"Credential in correct"
-             })
-             return 
-            } else {
-                const token = jwt.sign({
-                    username 
-                },JWT_SECRET)
-                res.json({
-                    token : token
-                })
-            }
-     
-})
-
-function auth(req,res,next){
-    const token = req.header.token
-    const decodedData = jwt.verify(token,JWT_SECRET)
-
-    if(decodedData.username){
-    next()
+    if (!foundUser) {
+        return res.json({
+            message: "Credentials are incorrect"
+        });
     } else {
-        res.jsom({
-        })
+        const token = jwt.sign({ username }, JWT_SECRET);
+        res.json({ token });
+    }
+});
+
+// Auth middleware
+function auth(req, res, next) {
+    const token = req.headers.token; // Get token from headers
+
+    if (!token) {
+        return res.status(401).json({ message: "Token missing" });
+    }
+
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        if (decodedData.username) {
+            next(); // Call next middleware if token is valid
+        } else {
+            res.status(401).json({ message: "Unauthorized" });
+        }
+    } catch (err) {
+        res.status(401).json({ message: "Invalid token" });
     }
 }
 
-
-
-app.get("/me", logger,auth, (req,res)=>{
+// Route for user profile
+app.get("/me", auth, (req, res) => {
+    const token = req.headers.token; // Use token from headers
+    const userDetails = jwt.verify(token, JWT_SECRET);
     
-       let foundUser = null;
+    const username = userDetails.username;
+    const user = users.find(user => user.username === username);
 
-     for(let i =0 ; i<users.length; i++){
-        if(users[i].username === decodedData.username ){
-            foundUser = users[i]
-        }
-     }
-
-     res.json({
-        username:foundUser.username,
-        password:foundUser.password,
-        
-     })
+    if (user) {
+        res.send({ username: user.username });
+    } else {
+        res.status(401).send({ message: "Unauthorized" });
     }
+});
 
-    
-    
-)
-
-    app.get("/todo", auth, (req,res)=>{
-
-    })
-
-    app.post("/todo", auth, (req,res)=>{
-
-    })
-
-    app.delete("/todo", auth, (req,res)=>{
-
-    })
-
-app.listen(3000,
-    console.log("Server start")
-)
+// Start the server
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
