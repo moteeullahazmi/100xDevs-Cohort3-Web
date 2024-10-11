2ae821695ca01132532e97e6a3c90e8307bdf842
moteeullahazmi <61102569+moteeullahazmi@users.noreply.github.com>
2024-10-11 18:36:49 +0530

    Week 7.1 complete

---
+++ Week 07 - MongoDB/7.1 - MongoDB/todo-app/index.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const app = express()
+
+const jwt = require("jsonwebtoken")
+const {auth,JWT_SECRET} =  require("./auth")
+
+// mongoose 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/Moteeullah-Azmi-data")
+
+// importing schema
+const {UserModel,TodoModel} = require("./db")
+
+// importing auth
+
+app.use(express.json())
+app.post("/signup", async (req,res) =>{
+   const  name = req.body.name;
+   const  email = req.body.email;
+   const  password = req.body.password;
+
+   await UserModel.create({
+    name:name,
+    password:password,
+    email:email
+   })
+
+   res.json({
+    message:"You are signed up"
+   })
+})
+
+app.post("/signin", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password,
+    });
+
+    console.log(user)
+
+    if (user) {
+        const token = jwt.sign({
+            id: user._id.toString()
+        },JWT_SECRET)
+
+        res.json({
+            token:token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentialfs"
+        })
+    }
+});
+
+app.post("/todo", auth, async (req,res) =>{
+   const userId = req.userId;
+   const title = req.body.title;
+   TodoModel.create ({
+    title,
+    userId
+   })
+
+   res.json({
+    userId: userId,
+   })
+})
+
+app.get("/todos", auth, async (req,res) =>{
+    const userId = req.userId;
+    const todos=  await TodoModel.find({
+        userId: userId
+    })
+    res.json({
+        todos
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("server start from 3000 port")
+})
