0f453ceacc1f6455057b221dfd8af4360f15e8d9
moteeullahazmi <moteeullahazmi@gmail.com>
2024-11-30 12:51:34 +0530

    typescript 39

---
+++ Week 14 - Typescript/14.1 - Basic Typescript/main.ts
@@ -0,0 +1,11 @@
+function greet(firstName:string){
+console.log("Hello " + firstName)
+}
+
+greet("Moteeullah Azmi");
+
+function sum(first:number, second:number):number{
+    return first +second
+ }
+ 
+ console.log(sum(14,15))
