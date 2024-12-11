5e6fdf6ad8237a35b9ce918a88b33d1e37fd3c16
moteeullahazmi <moteeullahazmi@gmail.com>
2024-12-11 12:39:41 +0530

    week 15.1 completed

--- Week 15 - Brain App/Brainly/src/index.ts
+++ Week 15 - Brain App/Brainly/src/index.ts
@@ -29,15 +29,15 @@ app.post("/api/v1/signup", async (req, res) => {
 
 
 
-    } catch (error) {
-        res.status(411).json({
-            message: "User already exist"
+    } catch (error: any) {
+        res.json({
+            message: error.message
         })
     }
 })
 
 // signin
-app.post("/api/v1/signin", async (req,res) => {
+app.post("/api/v1/signin", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
@@ -46,47 +46,90 @@ app.post("/api/v1/signin", async (req,res) => {
         password: password
     })
 
-    if (existingUser) {
-        const token = jwt.sign({
-            id: existingUser._id
-        },JWT_PASSWORD )
-        res.json({
-           token: token
-        })
+    try {
+        if (existingUser) {
+            const token = jwt.sign({
+                id: existingUser._id
+            }, JWT_PASSWORD)
+            res.json({
+                token: token
+            })
 
-    } else {
-        res.status(403).json({
-            message: "Incorrect credentials"
+        } else {
+            res.status(403).json({
+                message: "Incorrect credentials"
+            })
+        }
+    } catch (error: any) {
+        console.log(error)
+        res.json({
+            message: error.message
         })
     }
 }
 )
 
 // Post Content
-app.post("/api/v1/content",userMiddleware, async(req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
     const link = req.body.link;
     const type = req.body.type;
-    await ContentModel.create({
-        link,
-        type,
-        // @ts-ignore
-        userId: req.userId,
-
-    })
-    res.json({
-        message: "Content added"
-    })
+    try {
+        await ContentModel.create({
+            link,
+            type,
+            // @ts-ignore
+            userId: req.userId
+        })
+        res.json({
+            message: "Content added"
+        })
+    } catch (error: any) {
+        console.log(error)
+        res.json({
+            message: error.message
+        })
+    }
 })
 
 // fetching all data 
-app.get("/api/v1/content", (req, res) => {
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
 
+    try {
+        // @ts-ignore
+        const userId = req.userId;
+        const content = await ContentModel.find({
+            userId
+        }).populate("userId", "username")  //foreign key  and select
+        res.json({
+            content
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            // @ts-ignore
+            message: error.message
+        })
+    }
 })
 
 
 // deleting
-app.delete("/api/v1/content", async (req, res) => {
-
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
+    try {
+        await ContentModel.deleteMany({
+            _id:contentId,
+            // @ts-ignore
+            userId: req.userId
+        })
+        res.json({
+            message:"Delete Successful",contentId
+        })
+    } catch (error:any) {
+        res.json({
+            message: error.message
+        })
+    }
 })
 
 // sharable link
--- Week 15 - Brain App/Brainly/src/middleware.ts
+++ Week 15 - Brain App/Brainly/src/middleware.ts
@@ -7,13 +7,20 @@ export const userMiddleware = (req: Request, res: Response, next: NextFunction) 
     const header = req.headers["authorization"];
     const decoded = jwt.verify(header as string,JWT_PASSWORD);
 
-    if(decoded){
-        // @ts-ignore
-        req.userId = decoded.id
-        next();
-    } else{
-        res.status(403).json({
-            message: "You are not login"
+    try {
+        if(decoded){
+            // @ts-ignore
+            req.userId = decoded.id
+            next();
+        } else{
+            res.status(403).json({
+                message: "You are not login"
+            })
+        }
+    } catch (error:any) {
+        console.log(error)
+        res.json({
+            message: error.message
         })
     }
 
